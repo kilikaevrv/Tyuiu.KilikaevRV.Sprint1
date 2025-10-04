db8843a54a4a41e51b158577ad6fe5e72163df5e
Kilikaevrv <coffeine5555@gmail.com>
2025-10-05 00:17:13 +0300

    Добавьте файлы проекта.

---
+++ Tyuiu.KilikaevRV.Sprint1.Task5.V4.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KilikaevRV.Sprint1.Task5.V4.Lib
+{
+    public class DataService
+    {
+        public double Calculate(double k)
+        {
+            return Math.Round(k / 3600);
+        }
+    }
+}

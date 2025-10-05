3a536b03290555de0beda7c37e05c0624ebb7f46
Kilikaevrv <coffeine5555@gmail.com>
2025-10-05 22:28:09 +0300

    Task5

---
+++ Tyuiu.KilikaevRV.Sprint1.Task5.V4.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KilikaevRV.Sprint1.Task5.V4.Lib
+{
+    public class DataService : ISprint1Task5V4
+    {
+        public double Calculate(double k)
+        {
+            return Math.Round(k / 3600);
+        }
+
+        public int SecondsToHours(int time)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+}
---
+++ Tyuiu.KilikaevRV.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.KilikaevRV.Sprint1.Task5.V4.Lib;
+
+namespace Tyuiu.KilikaevRV.Sprint1.Task5.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1| Выполнил: Киликаев Р.В. | ИБКСБ-25-1";
+
+            //Длинна строки 75 символов
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("Спринт #1");
+            Console.WriteLine("Тема: Базовые навыки работы в C#");
+            Console.WriteLine("Задание #1");
+            Console.WriteLine("Вариант #8");
+            Console.WriteLine("Выполнил: Киликаев Роман Валерьевич | ИБКСБ-25-1");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("УСЛОВИЕ:");
+            Console.WriteLine("Написать программу, которая запрашивает у пользователя исходные данные, вычисляет результат по формуле (x*Pi)/a и печатает его на экране.");
+            Console.WriteLine("и печатает результат на экране.");
+            Console.WriteLine("");
+            Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ:");
+            Console.WriteLine("********************************************************************************************************");
+            Console.WriteLine("* 10 / (2 + 3                                                                                          *");
+            Console.WriteLine("* результат                                                                                            *");
+
+            Console.ReadKey();
+        }
+    }
+}

f6fdb3b8a64b426fe6ef97f939fcbb2d78cf7250
Kilikaevrv <coffeine5555@gmail.com>
2025-10-05 21:52:25 +0300

    Task6

--- Tyuiu.KilikaevRV.Sprint1.Task6.V1.Lib/DataService.cs
+++ Tyuiu.KilikaevRV.Sprint1.Task6.V1.Lib/DataService.cs
@@ -1,29 +1,27 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
 
+using System;
+
 namespace Tyuiu.KilkaevRV.Sprint1.Task6.V1.Lib
 {
     public class DataService : ISprint1Task6V1
     {
-        public string GetCharCode(char symbol)
+{
+        public string SymbolCode(string value)
         {
+            throw new NotImplementedException();
+        }
+    }
 
+    public string GetCharCode(char symbol)
+        {
             if (symbol == '.')
             {
                 return "exit";
             }
 
             int code = (int)symbol;
-            return code.ToString();
-        }
-
-        public string? GetCharCode(int v)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string SymbolCode(string value)
-        {
-            throw new NotImplementedException();
+            return code.ToString(); // Возвращаем только код как строку
         }
     }
 }

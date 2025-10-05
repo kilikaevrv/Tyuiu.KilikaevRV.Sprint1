a6d213b04b53fac288c98d3a122d13364d49157f
Kilikaevrv <coffeine5555@gmail.com>
2025-10-05 23:29:22 +0300

    Task5

--- Tyuiu.KilikaevRV.Sprint1.Task5.V4.Lib/DataService.cs
+++ Tyuiu.KilikaevRV.Sprint1.Task5.V4.Lib/DataService.cs
@@ -1,16 +1,22 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint1;
-namespace Tyuiu.KilikaevRV.Sprint1.Task5.V4.Lib
-{
-    public class DataService : ISprint1Task5V4
+
+
+ 
+
+namespace Tyuiu.KilkaevRV.Sprint1.Task5.V4.Lib
     {
-        public double Calculate(double k)
+        public class DataService : ISprint1Task5V4
         {
-            return Math.Round(k / 3600);
-        }
+            public int Calculate(double k)
+            {
+                // Целочисленное деление для получения полных часов
+                int hours = (int)(k / 3600);
+                return hours;
+            }
 
         public int SecondsToHours(int time)
         {
             throw new NotImplementedException();
         }
     }
-}
+    }
--- Tyuiu.KilikaevRV.Sprint1.Task5.V4.Test/DataServiceTest.cs
+++ Tyuiu.KilikaevRV.Sprint1.Task5.V4.Test/DataServiceTest.cs
@@ -1,5 +1,7 @@
 ﻿using Tyuiu.KilikaevRV.Sprint1.Task5.V4.Lib;
-namespace Tyuiu.KilikaevRV.Sprint1.Task5.V4.Test
+
+
+namespace Tyuiu.KilkaevRV.Sprint1.Task5.V4.Test
 {
     [TestClass]
     public sealed class DataServiceTest
@@ -8,9 +10,24 @@ namespace Tyuiu.KilikaevRV.Sprint1.Task5.V4.Test
         public void ValidExpression()
         {
             DataService ds = new DataService();
+            double k = 18000; // 5 часов
+            var res = ds.Calculate(k);
+            Assert.AreEqual(5, res);
+        }
+
+        [TestMethod]
+        public void ValidExpression13257()
+        {
+            DataService ds = new DataService();
             double k = 13257;
             var res = ds.Calculate(k);
             Assert.AreEqual(3, res);
         }
-    }
-}
+
+        [TestMethod]
+        public void ValidExpression3599()
+        {
+            DataService ds = new DataService();
+            double k = 3599; // Меньше часа
+            var res = ds.Calculate(k);
+            Assert.AreEqual(0, res

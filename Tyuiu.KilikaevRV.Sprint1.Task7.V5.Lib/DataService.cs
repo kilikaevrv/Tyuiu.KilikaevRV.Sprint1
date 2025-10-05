04f4a1fcdb7c165f04a466564c712ae87f3903ec
Kilikaevrv <coffeine5555@gmail.com>
2025-10-05 22:21:27 +0300

    Task7

---
+++ Tyuiu.KilikaevRV.Sprint1.Task7.V5.Lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.KilkaevRV.Sprint1.Task7.V5.Lib
+{
+    public class DataService : ISprint1Task7V5
+    {
+        public double Calculate(double x)
+        {
+            double numerator = Math.Log(Math.Abs(Math.Cos(x)));
+            double denominator = Math.Log(1 + Math.Pow(x, 2));
+            double result = numerator / denominator;
+
+            return Math.Round(result, 3);
+        }
+    }
+}
---
+++ Tyuiu.KilikaevRV.Sprint1.Task7.V5.Test/DataServiceTest.cs
@@ -0,0 +1,45 @@
+﻿
+using Tyuiu.KilkaevRV.Sprint1.Task7.V5.Lib;
+
+namespace Tyuiu.KilkaevRV.Sprint1.Task7.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest 
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            double x = 1.0;
+            double wait = -0.255;
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculateZero()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.0;
+            double wait = 0.0;
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(wait, res);
+        }
+
+        [TestMethod]
+        public void ValidCalculateNegative()
+        {
+            DataService ds = new DataService();
+
+            double x = -1.5;
+            double wait = -0.154;
+            double res = ds.Calculate(x);
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

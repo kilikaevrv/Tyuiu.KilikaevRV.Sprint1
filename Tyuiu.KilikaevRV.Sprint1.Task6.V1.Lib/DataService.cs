44f82fea3bfb146139b57c5a198bca78314d4cba
Kilikaevrv <coffeine5555@gmail.com>
2025-10-05 21:41:06 +0300

    Task6

---
+++ Tyuiu.KilikaevRV.Sprint1.Task6.V1.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KilkaevRV.Sprint1.Task6.V1.Lib
+{
+    public class DataService : ISprint1Task6V1
+    {
+        public string GetCharCode(char symbol)
+        {
+
+            if (symbol == '.')
+            {
+                return "exit";
+            }
+
+            int code = (int)symbol;
+            return $"Символ: {symbol} Код: {code}";
+        }
+
+        public string SymbolCode(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.KilikaevRV.Sprint1.Task6.V1.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿
+using Tyuiu.KilkaevRV.Sprint1.Task6.V1.Lib;
+
+namespace Tyuiu.KilkaevRV.Sprint1.Task6.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            // Тест для символа '1'
+            char symbol1 = '1';
+            var res1 = ds.GetCharCode(symbol1);
+            Assert.AreEqual("Символ: 1 Код: 49", res1);
+
+            // Тест для символа 'A'
+            char symbol2 = 'A';
+            var res2 = ds.GetCharCode(symbol2);
+            Assert.AreEqual("Символ: A Код: 65", res2);
+
+            // Тест для точки (сигнал завершения)
+            char symbol3 = '.';
+            var res3 = ds.GetCharCode(symbol3);
+            Assert.AreEqual("exit", res3);
+        }
+    }
+}
+
+
